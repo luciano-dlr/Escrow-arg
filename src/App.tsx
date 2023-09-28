1d158232189decaea2277b05ef6ceed9a3f5504a
Luciano de la Rubia <delarubialuciano@gmail.com>
2023-09-28 18:16:30 -0300

    Creating infraestructure and README

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { LandingPage } from './infraestructure/LandingPage/LandingPage'
+
+function App() {
+  
+
+  return (
+    <>
+      <LandingPage/>
+    </>
+  )
+}
+
+export default App
